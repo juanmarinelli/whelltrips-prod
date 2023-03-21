c8dfa1f9efb1740d9a12ac5ee355f08e110b1beb
juanmarinelli <juanmarinelli10@gmail.com>
2023-03-21 18:09:06 -0300

    asd

--- _nuxt/static/1679431792/indexv1/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1679431792",layout:"default",error:a,state:{productos:[],categories:[],cart:{cart:[],showCart:false},user:{users:[],user:a}},serverRendered:true,routePath:"\u002Findexv1",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
